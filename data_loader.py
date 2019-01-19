276d20be903b344d73344a8665a9272ebd897508
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-01-19 19:18:13 +0530

    Added code to load glove and preprocess glove, and create the vocabulary from the dataset

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,35 @@
 import torch
 import torch.utils.data as datautil
-import os
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-from utils import split_story
+import numpy as np
 
+from utils import split_story, make_target_vocab, load_glove_vectors
+from config import Config
+
+import os
+import sys
+import subprocess
 class TextDataset(): 
 	''' Dataset Loader for a text dataset'''
-	
-	
+		
+	def create_embedding_matrix(self,emb_dim):
+		matrix_len = self.config.VOCAB_SIZE
+		weights_matrix = np.zeros((matrix_len, emb_dim))
+		words_found = 0
+		target_vocab = make_target_vocab(self.config)
+		glove = load_glove_vectors(self.config)
+		for i, word in enumerate(target_vocab):
+		    try: 
+		        weights_matrix[i] = glove[word]
+		        words_found += 1
+		    except KeyError:
+		        weights_matrix[i] = np.random.normal(scale=0.6, size=(emb_dim, ))
 
 	def get_list_of_docs(self,root_dir):
 		return os.listdir(root_dir)
 
-	def __init__(self, root_dir,run_type=None, transform=None):
+	def __init__(self, config=None,run_type=None, transform=None):
 		"""
 		Args:
 			root_dir (string): Directory with all the images.
@@ -21,12 +37,14 @@ class TextDataset():
 				on a sample.
 		"""
 		# self.landmarks_frame = pd.read_csv(csv_file)
-		self.root_dir = os.path.abspath(root_dir)
+		self.root_dir = os.path.abspath(config.paths.PATH_TO_DATASET)
 		self.transform = transform
 		self.file_list = self.get_list_of_docs(self.root_dir)
 		print ("total length ",len(self.file_list))
 		print ("first 5 file names",self.file_list[:5])
-
+		if config is not None:
+			self.config = config
+		
 	def __len__(self):
 		return len(self.file_list)
 
@@ -37,16 +55,27 @@ class TextDataset():
 		story, highlights = split_story(text)
 		return story, highlights
 
-dataset = TextDataset("/home/codexetreme/Desktop/datasets/cnn_stories_tokenized")
+
+config = Config()
+config.paths.GLOVE_PATH = '/home/codexetreme/Desktop/Source/GloVe-1.2/build'
+config.paths.PROCESSED_GLOVE_PATH = '/home/codexetreme/Desktop/datasets/armageddon_dataset'
+config.paths.PATH_TO_CORPUS = '/home/codexetreme/Desktop/datasets/armageddon_dataset/corpus.txt'
+config.paths.PATH_TO_VOCAB_TXT = '/home/codexetreme/Desktop/datasets/armageddon_dataset/vocab.txt'
+config.paths.PATH_TO_DATASET = '/home/codexetreme/Desktop/datasets/armageddon_dataset/cnn_stories_tokenized'
+dataset = TextDataset(config=config)
 data_loader = datautil.DataLoader(dataset=dataset,batch_size=1,num_workers=4,shuffle=False)
 
-_i = 1
+create_vocabulary_from_dataset()
+dataset.create_embedding_matrix(100)
 
-for i,(s,h) in enumerate(data_loader):
-	print ("i = ", i)
-	print ("story", s)
-	print ("highlight", h)
-	print ("-"*50)
-	_i-=1
-	if _i<-1:
-		break
+
+# _i = 1
+
+# for i,(s,h) in enumerate(data_loader):
+# 	print ("i = ", i)
+# 	print ("story", s)
+# 	print ("highlight", h)
+# 	print ("-"*50)
+# 	_i-=1
+# 	if _i<-1:
+# 		break
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
-
-
+import bcolz
+import numpy as np
+import pickle
+import subprocess
 def split_story(doc):
 	# find first highlight
 	index = doc.find('@highlight')
@@ -9,3 +11,80 @@ def split_story(doc):
 	# strip extra white space around each highlight
 	highlights = [h.strip() for h in highlights if len(h) > 0]
 	return story, highlights
+
+
+
+def process_glove(glove_path,dims):
+	'''
+	This method writes 2 pkl files that store the word list from the glove 
+	vectors (~400K words) and a corresponding file that stores each words index
+	
+	:param glove_path: path to glove file
+	:param dims: the dimentions to use
+
+	this makes the total path as {glove_path}/glove.6B.{dims}d.txt
+
+	NOTE: bcolz stores a .dat folder that is used for compressing the large number of 
+	words that are present in the glove file
+
+	Run this function once to generate the respective pickle files.
+
+	'''
+	path = os.path.join(glove_path,'glove.6B.{dims}d.txt'.format(dims=dims))
+	dat_file_name = os.path.join(glove_path,'glove.6B.{dims}d.dat'.format(dims=dims))
+	print (path)
+	words = []
+	idx = 0
+	word2idx = {}
+	vectors = bcolz.carray(np.zeros(1), rootdir=dat_file_name, mode='w')
+
+	with open(path, 'rb') as f:
+	    for l in f:
+	        line = l.decode().split()
+	        word = line[0]
+	        words.append(word)
+	        word2idx[word] = idx
+	        idx += 1
+	        vect = np.array(line[1:]).astype(np.float)
+	        vectors.append(vect)
+	    
+	vectors = bcolz.carray(vectors[1:].reshape((400000, dims)), rootdir=dat_file_name, mode='w')
+	vectors.flush()
+	pickle.dump(words, open(f'{glove_path}/6B.100_words.pkl', 'wb'))
+	pickle.dump(word2idx, open(f'{glove_path}/6B.100_idx.pkl', 'wb'))
+
+# process_glove('/home/codexetreme/Desktop/datasets/',100)
+
+
+def load_glove_vectors(config):
+	glove_path = config.paths.PROCESSED_GLOVE_PATH
+	vectors = bcolz.open(os.path.join(glove_path,'glove.6B.100d.dat'))[:]
+	words = pickle.load(open(os.path.join(glove_path,'6B.100_words.pkl'), 'rb'))
+	word2idx = pickle.load(open(os.path.join(glove_path,'6B.100_idx.pkl'), 'rb'))
+	glove = {w: vectors[word2idx[w]] for w in words}
+	return glove
+# g = test_glove_vectors('/home/codexetreme/Desktop/datasets/armageddon_dataset/')
+
+# print(g['shanky'])
+
+def make_target_vocab(config):
+	# output_file = open('test.txt','w+')
+	# subprocess.call(['awk','{print $1}',config.paths.PATH_TO_VOCAB_TXT],stdout=output_file)
+	# subprocess.call(['mv','test.txt',config.paths.PATH_TO_VOCAB_TXT])
+	words = []
+	with open(config.paths.PATH_TO_VOCAB_TXT) as file:
+		for line in file.readlines():
+			words.append(line.split(' ')[0])
+	return words
+	# output_file.close()	
+
+def create_vocabulary_from_dataset(config):
+	cmd = [os.path.join(config.paths.GLOVE_PATH,"vocab_count"), "-min-count",str(config.MIN_VOCAB_COUNT)
+		,"-max-vocab",str(config.VOCAB_SIZE)]
+	corpus_file = open(config.paths.PATH_TO_CORPUS,'r')
+	vocab_file = open(config.paths.PATH_TO_VOCAB_TXT,'w+')
+	try:
+		subprocess.check_call(cmd,stdin=corpus_file,stdout=vocab_file)
+	finally:
+		corpus_file.close()
+		vocab_file.close()
