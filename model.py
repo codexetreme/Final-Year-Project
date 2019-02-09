53947c20840750c35c74d951a9107868723afb26
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-02-09 19:06:17 +0530

    Added D_i layer in document encoder. Also added minor changes to respond to the new config changes.

--- model.py
+++ model.py
@@ -30,9 +30,9 @@ class SentenceEncoder(nn.Module):
 
         Ks = [(1,40),(2,50),(3,50),(4,60),(5,60),(6,70),(7,70)]
         
-        self.convs = nn.ModuleList([nn.Conv1d(1,out_channels=out_c,kernel_size=(k,self.config.WORD_DIMENTIONS)) for (k,out_c) in Ks])
+        self.convs = nn.ModuleList([nn.Conv1d(1,out_channels=out_c,kernel_size=(k,self.config.dataset_options.WORD_DIMENTIONS)) for (k,out_c) in Ks])
     
-        self.max_pool = nn.MaxPool1d(kernel_size=self.config.MAX_SENTENCES_PER_DOCUMENT)
+        self.max_pool = nn.MaxPool1d(kernel_size=self.config.dataset_options.MAX_SENTENCES_PER_DOCUMENT)
 
         self.highway_layer = nn.ModuleList([Highway(input_size, 3, f=torch.nn.functional.relu) for i,input_size in Ks])
 
@@ -46,11 +46,11 @@ class SentenceEncoder(nn.Module):
 
     def forward(self,x):
         x = self.embedding_layer(x) # (N,doc_len,sentence_len,dims)
-        x = x.unsqueeze(1).view(-1,1,self.config.MAX_SENTENCES_PER_DOCUMENT,self.config.WORD_DIMENTIONS) # (N*doc_len,Ci,sentence_len,dims)
+        x = x.unsqueeze(1).view(-1,1,self.config.dataset_options.MAX_SENTENCES_PER_DOCUMENT,self.config.dataset_options.WORD_DIMENTIONS) # (N*doc_len,Ci,sentence_len,dims)
 
         x = [self.conv_and_pool(x,i) for i in self.convs]
         x = torch.cat(x,dim=1)
-        x = x.view(self.config.BATCH_SIZE,-1,400)
+        x = x.view(self.config.globals.BATCH_SIZE,-1,400)
         print ('x_shape:', x.shape)
             
         return x
@@ -60,12 +60,20 @@ class SentenceEncoder(nn.Module):
 class DocumentEncoder(nn.Module):
 
     def __init__(self,config=None):
+        '''
+        D_i => the layer that calulates the document score, it is implemented as presented in the paper.
+        '''
         super(DocumentEncoder,self).__init__()
-        print('GRU------------------')
         if config is not None:
             self.config = config
-        self.gru_layer = nn.GRU(input_size=400,hidden_size=400,num_layers=1,batch_first=True,bidirectional=True)
-
+        hidden_size = 50
+        self.gru_layer = nn.GRU(input_size=400,hidden_size=hidden_size,num_layers=1,batch_first=True,bidirectional=True)
+        self.D_i = nn.Sequential(
+                # Due to bi directional, we multipl the hidden size by 2
+                nn.Linear(2*hidden_size,2*hidden_size),
+                nn.BatchNorm1d(2*hidden_size),
+                nn.Tanh()
+                )
 
     def forward(self,x):
         '''
@@ -74,16 +82,16 @@ class DocumentEncoder(nn.Module):
         H_i = output, these are the vectors that are represent the hidden states
         
         Shapes 
-        X = (N,num_sentences, sentence_features[=400]) 400 is the features for the sentence here
+        X = (N,num_sentences, sentence_features[=50]) 50 is the features for the sentence here
         D_i => (N,hidden_size*2[since it is bidirectional])
-        H_i = (N,num_sentences,hidden_size)
+        H_i => (N,num_sentences,hidden_size*2[since it is bidirectional])
         
         Returns
         D_i,output(ie,H_i)
         '''
         output,h_n = self.gru_layer(x)
-        D_i = torch.tanh(h_n.view(self.config.BATCH_SIZE,-1))
+        D_i = self.D_i(h_n.view(self.config.BATCH_SIZE,-1))
         print ('Document_encoder_shape',D_i.shape)
-        print ('h_n_shape',h_n.shape)
+        print ('output_shape',output.shape)
         return D_i,output
 
